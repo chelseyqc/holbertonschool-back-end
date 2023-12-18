8b4455f229db2652e9658c42075e6cd31ae0c689
chelseyqc <6721@holbertonstudents.com>
2023-12-18 13:58:53 +0800

    feat: extended script for all employees in json format

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+returns info about all employee's todo list progress,
+exports data in the json format
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Base URL
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # fetching user data
+    users_url = base_url + "/users"
+    user_data = requests.get(users_url).json()
+
+    all_tasks = {}
+
+    # fetching tasks and todos
+    for user in user_data:
+        USERNAME = user['username']
+        USER_ID = user['id']
+
+        # fetching todos for the user
+        todos_url = base_url + "/users/" + str(USER_ID) + "/todos"
+        todos_list = requests.get(todos_url).json()
+
+        # json export data
+        tasks = [{
+            "username": USERNAME,
+            "task": todo['title'],
+            "completed": todo['completed']
+        } for todo in todos_list]
+
+        all_tasks[USER_ID] = tasks
+
+    # write data to json
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(all_tasks, jsonfile)
