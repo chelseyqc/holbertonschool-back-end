c4f2ac5c32a2d6789aa83053d1811c30086c1ddc
chelseyqc <6721@holbertonstudents.com>
2023-12-18 13:32:40 +0800

    fix: changed order of user data

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,24 +12,26 @@ if __name__ == "__main__":
     # Base URL
     base_url = "https://jsonplaceholder.typicode.com"
     employee_id = str(sys.argv[1])
-    users_url = base_url + "/users/" + employee_id
-    EMPLOYEE_NAME = requests.get(users_url).json().get('name')
 
+    # fetching user data
+    users_url = base_url + "/users/" + employee_id
+    user_data = requests.get(users_url).json()
+    EMPLOYEE_NAME = user_data.get('name')
+    USERNAME = user_data.get('username')
+    USER_ID = user_data['id']
+
+    # fetching todos
     todos_url = base_url + "/users/" + employee_id + "/todos"
     todos_list = requests.get(todos_url).json()
     TOTAL_NUMBER_OF_TASKS = len(todos_list)
-
     NUMBER_OF_DONE_TASKS = len([todo for todo in todos_list
                                 if todo['completed']])
-    users = requests.get(users_url).json()
-    USER_ID = users['id']
     # open csv file to write
     with open('{}.csv'.format(USER_ID), mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
         # write rows in required format
         for todo in todos_list:
-            USERNAME = users['username']
             TASK_COMPLETED_STATUS = todo['completed']
             TASK_TITLE = todo['title']
             writer.writerow([USER_ID,
