859578fb38542906e12783003bb56d54e17a586b
chelseyqc <6721@holbertonstudents.com>
2023-12-18 12:31:17 +0800

    feat: added a script to return employee info and todo list progress

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Base URL
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_id = str(sys.argv[1])
+    users_url = base_url + "/users/" + employee_id
+    EMPLOYEE_NAME = requests.get(users_url).json().get('name')
+
+    todos_url = base_url + "/users/" + employee_id + "/todos"
+    todos_list = requests.get(todos_url).json()
+    TOTAL_NUMBER_OF_TASKS = len(todos_list)
+
+    NUMBER_OF_DONE_TASKS = len([todo for todo in todos_list
+                                if todo['completed']])
+
+    # print progress
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
+    ))
+
+    # print title of completed tasks
+    for todo in todos_list:
+        if todo['completed']:
+            print("\t {}".format(todo['title']))
